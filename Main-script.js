586fbea2e95e8b66a2afc8b73c3fe68da0d90e92
K70123 <krall.sasongko@gmail.com>
2025-06-24 17:21:34 +0700

    Update Main-script.js

--- Main-script.js
+++ Main-script.js
@@ -4,6 +4,8 @@ let appData = JSON.parse(localStorage.getItem('chordAppData'));
 function saveData() {
   localStorage.setItem('chordAppData', JSON.stringify(appData));
 }
+localStorage.clear()
+location.reload()
 
 window.keyList = [
   'A', 'A#', 'Bb', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'Num'
